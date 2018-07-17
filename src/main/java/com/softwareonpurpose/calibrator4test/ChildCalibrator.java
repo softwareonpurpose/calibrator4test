9a609aa2fb88c80a55dea218004621a3a652ddb8
Craig Stockton <cstockto@teksystems.com>
2018-07-17 11:57:39 -0700

    Delete Child and Root Calibrators

--- src/main/java/com/softwareonpurpose/calibrator4test/ChildCalibrator.java
+++
@@ -1,7 +0,0 @@
-package com.softwareonpurpose.calibrator4test;
-
-public abstract class ChildCalibrator extends Calibrator{
-    protected ChildCalibrator(String description, Object expected, Object actual, Calibrator parent) {
-        super(description, expected, actual, parent);
-    }
-}
