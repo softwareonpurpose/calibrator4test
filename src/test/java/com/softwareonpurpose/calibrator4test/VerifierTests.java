56f20f5f252239c1eb5f32f005b25f4a845df826
Carlos Ibarra <carlos.ibarra@teksystemsgs.com>
2022-03-14 13:16:43 -0700

    Added Jacoco for coverage

--- src/test/java/com/softwareonpurpose/calibrator4test/VerifierTests.java
+++ src/test/java/com/softwareonpurpose/calibrator4test/VerifierTests.java
@@ -9,21 +9,21 @@ public class VerifierTests {
     @Test
     public void testVerify_expectedAndActualNull() {
         String expected = Verifier.PASS;
-        String actual = Verifier.verify("Verification description", null, null, null);
+        String actual = Verifier.verify("Verification description", null, null, IndentManager.getInstance());
         Assert.assertEquals(actual, expected, "Failed when actual and expected both <null>");
     }
 
     @Test
     public void testVerify_expectedAndActualEqual() {
         String expected = Verifier.PASS;
-        String actual = Verifier.verify("Verification description", "String", "String", null);
+        String actual = Verifier.verify("Verification description", "String", "String", IndentManager.getInstance());
         Assert.assertEquals(actual, expected, "Failed when actual is equal to expected");
     }
 
     @Test
     public void testVerify_expectedAndActualDifferent() {
         String expected = String.format("Verification description -- Expected: String  Actual: 999%n");
-        String actual = Verifier.verify("Verification description", "String", 999, null);
+        String actual = Verifier.verify("Verification description", "String", 999, IndentManager.getInstance());
         Assert.assertEquals(actual, expected, "Failed to return expected message when actual and expected are different");
     }
 
@@ -37,14 +37,14 @@ public class VerifierTests {
     @Test
     public void testVerify_expectedNull() {
         String expected = "Expected: <null>";
-        String actual = Verifier.verify("Verification description", null, 999, null);
+        String actual = Verifier.verify("Verification description", null, 999, IndentManager.getInstance());
         Assert.assertTrue(actual.contains(expected), "Failed to include 'Expected: <null>' in message");
     }
 
     @Test
     public void testVerify_actualNull() {
         String expected = "Actual: <null>";
-        String actual = Verifier.verify("Verification description", 999, null, null);
+        String actual = Verifier.verify("Verification description", 999, null, IndentManager.getInstance());
         String message = "Failed to include 'Actual: <null>' in message \"%s\"";
         Assert.assertTrue(actual.contains(expected), String.format(message, actual));
     }
